b3c992dceebccd70734d5e6a55af9627732807f1
dotZQ <76562714+dotZQ@users.noreply.github.com>
2020-12-24 23:46:20 +0700

    Update Account.cpp

--- SlotMachine/Account.cpp
+++ SlotMachine/Account.cpp
@@ -1,11 +1,3 @@
-//
-//  Account.cpp
-//  SlotMachine
-//
-//  Created by ZQ314159 on 17/7/2563 BE.
-//  Copyright © 2563 ZQ314159. All rights reserved.
-//
-
 #include "Account.hpp"
 Account::Account()
 {
